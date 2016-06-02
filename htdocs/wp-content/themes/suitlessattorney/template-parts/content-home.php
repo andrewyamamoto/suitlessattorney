865ed9aeb7639df6df46c260efe65de27c731d85
andrewyamamoto <andrew.asada@gmail.com>
2016-06-02 12:32:37 -0700

    Homepage edits and changes
    Added bullets and information to the webpage

--- htdocs/wp-content/themes/suitlessattorney/template-parts/content-home.php
+++ htdocs/wp-content/themes/suitlessattorney/template-parts/content-home.php
@@ -10,37 +10,46 @@
 ?>
 
 
-<section id="intro" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php
-		the_content( sprintf(
-			/* translators: %s: Name of current post. */
-			wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'suitlessattorney' ), array( 'span' => array( 'class' => array() ) ) ),
-			the_title( '<span class="screen-reader-text">"', '"</span>', false )
-		) );
-	?>
+<section id="intro">
+
+	<article class="">
+		<div class="container">
+			<div class="row">
+				<h1>
+					<?php
+						if ( get_field('suitlessattorney_intro_title') ) {
+							the_field('suitlessattorney_intro_title');
+						}
+					?>
+				</h1>
+			</div>
+		</div>
+	</article>
+
 	<article class="content">
 		<div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2">
-
+					<?php
+						if ( get_field('suitlessattorney_intro_content') ) {
+							the_field('suitlessattorney_intro_content');
+						}
+					?>
                 </div>
             </div>
-<?php
-if ( get_field('suitlessattorney_intro_content') ) {
-	the_field('suitlessattorney_intro_content');
-}
-?>
+
 		</div>
 	</article>
 </section>
+
 <section id="howitworks">
 
 	<h1>
-	<?php
-		if ( get_field('suitlessattorney_aside_title') ) {
-			the_field('suitlessattorney_aside_title');
-		}
-	?>
+		<?php
+			if ( get_field('suitlessattorney_aside_title') ) {
+				the_field('suitlessattorney_aside_title');
+			}
+		?>
 	</h1>
 	<div class='container'>
 
@@ -49,28 +58,40 @@ if ( get_field('suitlessattorney_intro_content') ) {
 				// check if the repeater field has rows of data
 				if( have_rows('suitlessattorney_aside_bullet') ):
 					$count = 0;
-			?>
 
-			<?php
-			// loop through the rows of data
+					// loop through the rows of data
 					while ( have_rows('suitlessattorney_aside_bullet') ) : the_row();
 
-					$bullet = get_sub_field('bullet_item');
-					$bulletImage = get_sub_field('bullet_image');
-					// display a sub field value
-					if (!$count) {
-	                	echo '<div class="col-lg-2 col-lg-offset-1">';
-	              	}else{
-						echo '<div class="col-lg-2">';
-					}
-					?>
-						<div class='hiw-icon'><span class='number'>$bulletImage</span></div>
-						<?php echo "$bullet"; ?>
-					echo '</div>';
-					$count++;
-					endwhile;
+						$bullet = get_sub_field('bullet_item');
+						$bulletImage = get_sub_field('bullet_image');
 
-					else :
+						// display a sub field value
+			?>
+						<?php if (!$count): ?>
+
+	                	<div class="col-lg-2 col-lg-offset-1 col-md-4 col-sm-4">
+
+	        			<?php else:?>
+
+						<div class="col-lg-2 col-md-4 col-sm-4">
+
+						<?php endif; ?>
+
+						<?php $count++;?>
+
+						<div class='item-container'>
+							<div class='hiw-icon'>
+								<span class='number'><?php echo "$count"; ?></span>
+								<?php echo "$bulletImage"; ?>
+
+							</div>
+							<span class='title'><?php echo "$bullet"; ?></span>
+						</div>
+
+					</div>
+					<?php
+					endwhile;
+					else:
 
 						// no rows found
 
@@ -82,7 +103,7 @@ if ( get_field('suitlessattorney_intro_content') ) {
 </section>
 
 <?php
-	if ( get_field('suitlessattorney_intro_content') ) {
-		the_field('suitlessattorney_intro_content');
-	}
+	// if ( get_field('suitlessattorney_intro_content') ) {
+	// 	the_field('suitlessattorney_intro_content');
+	// }
 ?>
